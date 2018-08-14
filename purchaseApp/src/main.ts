0648509c739038998d52e731a121838bad0f28ef
katrina <0993262626g@gmail.com>
2018-08-14 18:30:21 +0300

    shablon

--- purchaseApp/src/main.ts
+++ purchaseApp/src/main.ts
@@ -1,4 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+//импортирует функциональность модуля platformBrowserDynamic
+// из пакета angular/platform-browser-dynamic. 
+//platformBrowserDynamic использует bootstrapModule 
+//для загрузки нужного модуля.
 import { AppModule } from './app/app.module';
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
