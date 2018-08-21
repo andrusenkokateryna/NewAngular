a52d599ac7f2b446c23f525bd6773eebe8c6fbd6
katrina <0993262626g@gmail.com>
2018-08-21 13:11:35 +0300

    directive

--- practicAngular/angularPactic/src/app/data.component.ts
+++ practicAngular/angularPactic/src/app/data.component.ts
@@ -1,1 +1,25 @@
+import { Component, OnInit} from '@angular/core';
+import {DataService} from './data.service';
+import {LogService} from './log.service';
+import {Phone} from './phone';
 
+@Component({
+    selector: 'data-comp',
+    templateUrl: './data.component.html',
+    styleUrls: ['./app.component.css'],
+     providers: [DataService, LogService]
+})
+
+export class DataComponent implements OnInit { 
+     
+    items1: Phone[] = [];
+    constructor(private dataService: DataService){}
+     
+    addItem1(name1: string, price: number){
+         
+        this.dataService.addData(name1, price);
+    }
+    ngOnInit(){
+        this.items1 = this.dataService.getData();
+    }
+}
--- practicAngular/angularPactic/src/app/phone.ts
+++ practicAngular/angularPactic/src/app/phone.ts
@@ -2,3 +2,8 @@ export class Phone{
      
     constructor(public name1: string, public price: number) { }
 }
+
+
+
+
+
